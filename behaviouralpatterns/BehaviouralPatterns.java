b4397d6ae14e1ffffe95c6a5c3e2de5411c6ca58
Dave Bingham <dave@devbox.localdomain>
2024-02-28 20:19:39 +0000

    fixed typo

--- behaviouralpatterns/BehaviouralPatterns.java
+++ behaviouralpatterns/BehaviouralPatterns.java
@@ -42,7 +42,7 @@ public class BehaviouralPatterns
         
         System.out.println("cost: " + shippingCost);
         }
-    */
+    
     /*
     Template demo.
     public static void main(String[] args)
